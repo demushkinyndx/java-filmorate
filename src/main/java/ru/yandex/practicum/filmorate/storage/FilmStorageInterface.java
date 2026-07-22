e5bbf20edb6f31ef7ed08e8495b159dc445d7419
Andrey Demushkin <demushkin@gmail.com>
2026-07-22 09:52:32 +0300

    add-friends-likes новые интерфейсы для FilmStore и UserStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorageInterface.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorageInterface.java
@@ -2,8 +2,10 @@ package ru.yandex.practicum.filmorate.storage;
 
 import java.util.Collection;
 
-public interface UserStorage<T> {
+public interface FilmStorageInterface<T> {
     Collection<T> findAll();
+
+    T getById(int id);
 
     T create(T entity);
 
